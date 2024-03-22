11c0193a03743b2733ba5b5bf82ab14515fb872a
Alan Park <yuni070702@gmail.com>
2024-03-22 11:24:44 -0400

    changed cossim, app.py

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,9 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 LOCAL_MYSQL_USER = "root"
-LOCAL_MYSQL_USER_PASSWORD = "admin"
+LOCAL_MYSQL_USER_PASSWORD = "root"
 LOCAL_MYSQL_PORT = 3306
-LOCAL_MYSQL_DATABASE = "kardashiandb"
+LOCAL_MYSQL_DATABASE = "cs4300_booksdb"
 
 mysql_engine = MySQLDatabaseHandler(LOCAL_MYSQL_USER,LOCAL_MYSQL_USER_PASSWORD,LOCAL_MYSQL_PORT,LOCAL_MYSQL_DATABASE)
 
@@ -22,6 +22,8 @@ mysql_engine = MySQLDatabaseHandler(LOCAL_MYSQL_USER,LOCAL_MYSQL_USER_PASSWORD,L
 mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
+
+
 CORS(app)
 
 # Sample search, the LIKE operator in this case is hard-coded, 
